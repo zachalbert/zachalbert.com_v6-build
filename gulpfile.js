402c2708e44dcfea50ab0588c59ef588f6f787b3
zachalbert <zachalbert@gmail.com>
2014-11-23 18:51:10 -0800

    Port to a jekyll build, using gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,62 @@
 "use strict";
-/* global require */
 
-var gulp = require('gulp');
-var autoprefixer = require('gulp-autoprefixer');
-var concat = require('gulp-concat');
-var connect = require('gulp-connect');
-var mainBowerFiles = require('main-bower-files');
-var minifyCSS = require('gulp-minify-css');
-var notify = require('gulp-notify');
-var plumber = require('gulp-plumber');
-var rename = require('gulp-rename');
-var sass = require('gulp-sass');
-var uglify = require('gulp-uglify');
+var gulp            = require('gulp');
+var browserSync     = require('browser-sync');
+var autoprefixer    = require('gulp-autoprefixer');
+var concat          = require('gulp-concat');
+var connect         = require('gulp-connect');
+var mainBowerFiles  = require('main-bower-files');
+var minifyCSS       = require('gulp-minify-css');
+var notify          = require('gulp-notify');
+var plumber         = require('gulp-plumber');
+var rename          = require('gulp-rename');
+var sass            = require('gulp-sass');
+var uglify          = require('gulp-uglify');
+var cp              = require('child_process');
+var deploy          = require('gulp-gh-pages');
 
-var onError = function(e){
-  console.log(e);
-  // console.log(notify.through(function () {
-  //     this.emit(
-  //       "error", 
-  //       new Error("Something happened: DOOOOOOooomM!")
-  //     )}));
-};
+var messages = {
+  jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
+}
 
 
-// Compile Our Sass
+// Build site
+gulp.task('jekyll-build', function(done) {
+  browserSync.notify(messages.jekyllBuild);
+  return cp.spawn('jekyll', ['build'], {stdio: 'inherit'})
+    .on('close', done);
+});
+
+
+// Rebuild Jekyll & do page reload
+gulp.task('jekyll-rebuild', ['jekyll-build'], function() {
+  browserSync.reload();
+});
+
+
+// Wait for jekyll-build, then launch server
+gulp.task('browser-sync', ['styles', 'scripts', 'jekyll-build'], function() {
+  browserSync({
+    server: {
+      baseDir: '_site'
+    }
+  });
+});
+
+
+// Compile sass
 gulp.task('styles', function() {
-  return gulp.src('scss/style.scss')
-    .pipe(plumber(onError))
-    .pipe(sass({ style: 'expanded' }))
+  return gulp.src('_scss/style.scss')
+    .pipe(sass({ 
+      style: 'expanded',
+      includePaths: ['scss'],
+      onError: browserSync.notify
+    }))
     .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
     .pipe(minifyCSS({ keepBreaks: false }))
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest('_site/css'))
+    .pipe(browserSync.reload({stream: true}))
+    .pipe(gulp.dest('css'));
 });
 
 
@@ -41,26 +67,32 @@ gulp.task('scripts', function() {
     bowerrc: '.bowerrc',
     bowerJson: 'bower.json'
   };
-  return gulp.src(mainBowerFiles(bowerPaths).concat('js/*.js'))
-    .pipe(concat('all.js'))
-    .pipe(gulp.dest('dist'))
-    .pipe(rename('all.min.js'))
+  return gulp.src(mainBowerFiles(bowerPaths).concat('_js/*.js'))
+    .pipe(concat('site.js'))
+    .pipe(gulp.dest('_site/js'))
+    .pipe(rename('site.min.js'))
     .pipe(uglify())
-    .pipe(gulp.dest('dist'));
+    .pipe(browserSync.reload({stream: true}))
+    .pipe(gulp.dest('js'));
 });
 
 
-// Run a local webserver and watch for changes
-gulp.task('default', function() {
-  gulp.start(['styles', 'scripts']);
-  gulp.start('default');
-  connect.server({
-    port: 8000
-  });
-  gulp.watch('scss/*.scss', ['styles']);
-  gulp.watch('js/*.js', ['scripts']);
+// Watch task
+gulp.task('watch', function() {
+  gulp.watch('_scss/*.scss', ['styles']);
+  gulp.watch('_js/*.js', ['scripts']);
+  gulp.watch(['index.html', '_layouts/*.html', '_posts/*'], ['jekyll-rebuild']);
 });
 
 
-// Build
-gulp.task('build', ['styles', 'scripts']);
+// Default
+gulp.task('default', ['browser-sync', 'watch']);
+
+
+// Deploy to github gulp-gh-pages
+gulp.task('deploy', ['jekyll-build'], function() {
+  return gulp.src('./_site/**/*')
+    .pipe(deploy({
+      branch: 'master'
+    }));
+});
